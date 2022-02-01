bf99765b9e82ad55cf40dcf9537b315f872fcd01
IgorShyian <igor.szyjan.work@gmail.com>
2022-02-01 18:05:50 +0100

    some change

--- zadanie 2/index.js
+++ zadanie 2/index.js
@@ -1,23 +1,25 @@
-const startButton1 = document.getElementById("button1");
-const submitButton = document.getElementById("button2");
-const endButton1 = document.getElementById("endButton1");
-const endButton2 = document.getElementById("endButton2");
-const screen2 = document.getElementById("screen2");
-const phoneNumber = document.getElementById("phoneNumber");
-const packageKod = document.getElementById("packageKod");
-const phoneNumberError = document.querySelector(".spanError");
-const packageKodError = document.getElementById("packageKodError");
-const modalWindow = document.getElementById("modalWindow");
+'use strict'
+
+const startButton1 = document.getElementById("button1")
+const submitButton = document.getElementById("button2")
+const endButton1 = document.getElementById("endButton1")
+const endButton2 = document.getElementById("endButton2")
+const screen2 = document.getElementById("screen2")
+const phoneNumber = document.getElementById("phoneNumber")
+const packageKod = document.getElementById("packageKod")
+const phoneNumberError = document.querySelector(".spanError")
+const packageKodError = document.getElementById("packageKodError")
+const modalWindow = document.getElementById("modalWindow")
 
 let kodReg = /^[0-9]{4}$/
 let phoneReg = /^[0-9]{3}\s[0-9]{3}\s[0-9]{3}$/
 
-screen2.style.display = "none";
-startButton1.style.display = "block";
+screen2.style.display = "none"
+startButton1.style.display = "block"
 
 startButton1.addEventListener("click", () => {
-    screen2.style.display = "block";
-    startButton1.style.display = "none";
+    screen2.style.display = "block"
+    startButton1.style.display = "none"
 });
 
 phoneNumber.addEventListener("input", (e) => {
@@ -25,50 +27,50 @@ phoneNumber.addEventListener("input", (e) => {
 });
 
 packageKod.addEventListener("input", (e) => {
-    e.target.value = e.target.value.substr(0, 4);
+    e.target.value = e.target.value.substr(0, 4)
 });
 
 submitButton.addEventListener("click", () => {
     if (!phoneReg.test(phoneNumber.value)) {
-        phoneNumberError.classList.remove("hide");
+        phoneNumberError.classList.remove("hide")
     } else {
-        phoneNumberError.classList.add("hide");
+        phoneNumberError.classList.add("hide")
     }
 
     if (!kodReg.test(packageKod.value)) {
-        packageKodError.classList.remove("hide");
+        packageKodError.classList.remove("hide")
     } else {
-        packageKodError.classList.add("hide");
+        packageKodError.classList.add("hide")
     }
 
     if (phoneReg.test(phoneNumber.value)) {
         if (kodReg.test(packageKod.value)) {
-            phoneNumberError.classList.add("hide");
-            packageKodError.classList.add("hide");
+            phoneNumberError.classList.add("hide")
+            packageKodError.classList.add("hide")
 
             submitButton.innerText = 'Loading...'
             phoneNumber.disabled = true;
             packageKod.disabled = true;
             setTimeout(() => {
                 submitButton.innerText = 'Odbierz paczkę'
-                modalWindow.style.display = "block";
+                modalWindow.style.display = "block"
                 phoneNumber.value = ''
                 packageKod.value = ''
-                phoneNumber.disabled = false;
-                packageKod.disabled = false;
+                phoneNumber.disabled = false
+                packageKod.disabled = false
             }, 2000)
         }
     }
 });
 
 endButton1.addEventListener("click", () => {
-    modalWindow.style.display = "none";
-    screen2.style.display = "none";
-    startButton1.style.display = "block";
+    modalWindow.style.display = "none"
+    screen2.style.display = "none"
+    startButton1.style.display = "block"
 });
 
 endButton2.addEventListener("click", () => {
-    modalWindow.style.display = "none";
+    modalWindow.style.display = "none"
 });
 
 
